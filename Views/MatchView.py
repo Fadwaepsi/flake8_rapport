e441cd6f11cd3b6a8bba7ca62aa65e0038e1581d
Fadwa Talik <fadwa.bayoudh@ecoles-epsi.net>
2024-04-30 13:29:54 +0200

    👌 IMPROVE: enlever les espaces de fichier Menu.py

--- Views/MatchView.py
+++ Views/MatchView.py
@@ -10,8 +10,7 @@ class MatchView:
         #     round_number (int): Le numéro du tour auquel appartient le match.
 
         # Returns:
-        #     int: Le choix de l'utilisateur pour le résultat du match (1, 2 ou 3).
-        
+        #     int: Le choix de l'utilisateur pour le résultat du match (1, 2 ou 3). 
         print(f"Match {match_number} Round {round_number} - Résultats")
         print(f"1 - {match.player1['first_name']} {match.player1['last_name']}")
         print(f"2 - {match.player2['first_name']} {match.player2['last_name']}")
@@ -22,7 +21,6 @@ class MatchView:
         return user_choice
 
     def get_user_choice(self):
-        
         # Invite l'utilisateur à entrer son choix (1, 2 ou 3) et valide l'entrée.
 
         # Returns:
